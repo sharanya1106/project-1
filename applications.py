d260484c79434213bee495c22405bc2a8d086e02
sharanya <sharanyarao49@msitprogram.net>
2020-04-15 21:49:32 +0530

    task3

--- applications.py
+++ applications.py
@@ -1,12 +1,19 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<h1>Hello World!</h1>'
-@app.route('/register')
+
+@app.route('/register', methods = ['POST', 'GET'])
+
 def hello():
-    return render_template("reg_page.html")
+    if request.method == 'POST':
+        res = request.form
+        return render_template("result.html",result = res)
+    else:
+        return render_template("reg_page.html")
 
 if __name__ =='__main__':
     app.run(debug = True)
