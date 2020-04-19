a8ec892fa9323d0c9891ffce6fbb68ee277eabc1
sharanya <sharanyarao49@msitprogram.net>
2020-04-19 19:49:06 +0530

    added alerts to the registration status

--- applications.py
+++ applications.py
@@ -27,18 +27,26 @@ Session = scoped_session(sessionmaker(bind=engine))
 session = Session()
 
 @app.route('/')
-# def test():
-#     return render_template("reg_page.html")
+def test():
+    return redirect("/register")
 
 @app.route('/register', methods = ['POST', 'GET'])
 def register_method():
     db.create_all()
     if request.method == 'POST':
         userdata = User(request.form["username"], request.form["pswd"])
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user is not None:
+
+            
+            var = "Error: User already exists. Try with another Username or Register with if you are new user"
+            return render_template("reg_page.html", msg =var)
         db.session.add(userdata)
         db.session.commit()
+        var = 'Registration Success'
         # res = request.form
         # print(res)
-        return render_template("reg_page.html")
+
+        return render_template("reg_page.html", msg1 = var)
     else:
         return render_template("reg_page.html")
